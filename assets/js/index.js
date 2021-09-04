fd3babb5a3208ba2e9781311c9001426fbf8fa85
Faisal <mfaisalhidayatullah@gmail.com>
2021-09-04 07:11:27 +0700

    style: finish homepage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,25 @@
+// Menu Toggle
 $('.menu-toggle-btn').click(function () {
 	$(this).toggleClass('bx-x');
 	$('.navigation-menu').toggleClass('active');
 });
 
+// Dropdown
 $('.dropdown-toggle').hover(function () {
 	$('.icon-dropdown').toggleClass('bxs-up-arrow');
+});
+
+// Counter
+$('.counter').each(function () {
+	const This = $(this);
+	$({ Count: This.text() }).animate(
+		{ Count: This.parent().attr('data-count') },
+		{
+			duration: 2000,
+			easing: 'linear',
+			step: function () {
+				This.text(Math.floor(this.Count));
+			},
+		},
+	);
 });
