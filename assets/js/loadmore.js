9781c1ba1ed41900a7ab72873191d734654b13c9
Faisal <mfaisalhidayatullah@gmail.com>
2021-09-20 20:26:39 +0700

    feat: add reset filter

--- assets/js/loadmore.js
+++ assets/js/loadmore.js
@@ -1,7 +1,21 @@
+// Event Page
 $('.card-event').slice(0, 6).show();
 $('.loadMore').on('click', function () {
 	$('.card-event:hidden').slice(0, 3).slideDown();
 	if ($('.card-event:hidden').length == 0) {
 		$('.loadMore').fadeOut('slow');
 	}
+});
+
+// Challenge Page
+$('.card-challenge').slice(0, 8).show();
+$('.seeall').on('click', function () {
+	$('.card-challenge:hidden').slice(0, 100).slideDown();
+	if ($('.card-challenge:hidden').length == 0) {
+		$('.seeall').fadeOut('slow');
+	}
+});
+
+$('#resetFilter').click(function () {
+	$(location).prop('href', 'challenge.html');
 });
