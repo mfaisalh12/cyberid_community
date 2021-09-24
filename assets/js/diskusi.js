4297572a8a8f090b05c52c13671bb64330938261
Faisal <mfaisalhidayatullah@gmail.com>
2021-09-24 19:00:06 +0700

    feat: add forum page

---
+++ assets/js/diskusi.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	$('.tab-content:first').show();
+	$('.tab-navigation li:first').addClass('active');
+
+	$('.tab-navigation li').click(function () {
+		index = $(this).index();
+		$('.tab-navigation li').removeClass('active');
+		$(this).addClass('active');
+		$('.tab-content').hide();
+		$('.tab-content').eq(index).show();
+	});
+});
